08d5ab30f5b4bcb270c8d39f6f1ea505838f725c
BranimirK <bkedacic@hotmail.com>
2018-03-28 13:49:18 +0200

    initial commit

---
+++ RUAP App/RUAP App/RUAP App.cpp
@@ -0,0 +1,12 @@
+// RUAP App.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	printf("Pozdrav iz aplikacije od Branimir Kedaèiæ \n");
+    return 0;
+}
+
